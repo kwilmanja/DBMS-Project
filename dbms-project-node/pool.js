72532007cd86d07e14723b311e932da83c555a0b
kwilmanja <kwilman.j@northeastern.edu>
2024-12-05 20:36:47 -0500

    environment variables

--- dbms-project-node/pool.js
+++ dbms-project-node/pool.js
@@ -1,9 +1,11 @@
 import mysql from "mysql2";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'Winwin22*',
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD,
     database: 'stories',
     waitForConnections: true,
     connectionLimit: 10,
